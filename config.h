375c1b0ccf3eea7ec5dfca097346906e57584e60
Dr. Christian Nöding <christian@noeding-online.de>
2022-11-09 20:34:38 +0100

    Added config.h

---
+++ config.h
@@ -0,0 +1,35 @@
+
+// general software config
+const char* versionstring = "v0.0.1 built on ";
+const char compile_date[] = __DATE__ " " __TIME__;
+
+//#define UseWiFi 1
+
+#ifdef UseWiFi
+  // we are using WiFi connection
+  const char* ssid = "MyVeryPrivateWiFiNetwork";
+  const char* password = "MyVeryPrivateWiFiNetworkPassword";
+#else
+  // we are using ethernet-port via W5500
+
+  // Enter a MAC address for your controller below.
+  // Newer Ethernet shields have a MAC address printed on a sticker on the shield
+  byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; //MAC
+#endif
+
+const char* host = "remko2mqtt";
+
+#define UseStaticIP 1
+#ifdef UseStaticIP
+  IPAddress local_IP(192, 168, 0, 42);
+  IPAddress gateway(192, 168, 0, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  IPAddress primaryDNS(192, 168, 0, 1);   //optional
+  IPAddress secondaryDNS(1, 1, 1, 1); //optional
+#endif
+
+// setup MQTT
+#define mqtt_id "remko2mqtt"
+#define mqtt_server "192.168.0.41"
+#define mqtt_serverport 1883
+const char *mqtt_mppt_topic = "remko/mxw/";
